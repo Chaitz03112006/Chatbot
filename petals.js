f18d585b3694bc18692b43b5a097219150d25d8e
Chaitz03112006 <chaitanyap.bg@gmail.com>
2025-08-09 12:28:17 +0530

    Update petals.js

--- petals.js
+++ petals.js
@@ -1,17 +1,42 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const numPetals = 20; // number of petals
-    const container = document.body;
+const canvas = document.getElementById('petalsCanvas');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-    for (let i = 0; i < numPetals; i++) {
-        const petal = document.createElement("img");
-        petal.src = "/static/petal.png";
-        petal.classList.add("petal");
+const petals = [];
+const petalImage = new Image();
+petalImage.src = '/static/petal.png';
 
-        petal.style.left = Math.random() * 100 + "vw";
-        petal.style.animationDuration = 5 + Math.random() * 5 + "s"; 
-        petal.style.opacity = Math.random();
-        petal.style.transform = `scale(${Math.random()})`;
-
-        container.appendChild(petal);
+class Petal {
+    constructor() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * -canvas.height;
+        this.size = Math.random() * 20 + 10;
+        this.speed = Math.random() + 0.5;
     }
-});
+    update() {
+        this.y += this.speed;
+        if (this.y > canvas.height) {
+            this.y = -this.size;
+            this.x = Math.random() * canvas.width;
+        }
+    }
+    draw() {
+        ctx.drawImage(petalImage, this.x, this.y, this.size, this.size);
+    }
+}
+
+for (let i = 0; i < 30; i++) {
+    petals.push(new Petal());
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    petals.forEach(petal => {
+        petal.update();
+        petal.draw();
+    });
+    requestAnimationFrame(animate);
+}
+
+petalImage.onload = animate;
